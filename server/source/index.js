aa9fed980e6833fc41f523c0c9a9d4a6d34c95cd
Harsh Patel <harshppatel2880@gmail.com>
2019-12-21 19:35:10 -0600

    Redoing the server

--- server/source/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  /* eslint-disable no-console */
-  console.log(`Server started at http://localhost:${PORT}`);
-  /* eslint-enable no-console */
-});
